55d89849106d985a7c3eacf064e9bc6da5628aa5
Gabby <Gabrielle@lawn-128-61-56-132.lawn.gatech.edu>
2019-02-16 12:48:36 -0500

    changes

--- project/start.js
+++ project/start.js
@@ -10,12 +10,25 @@ app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+      response.render('pages/index');
+    });
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
 // This file is what handles incoming requests and
